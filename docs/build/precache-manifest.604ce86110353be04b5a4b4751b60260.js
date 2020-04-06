626c0c431e0b798d8f296de9346777b3c3e38726
orYoffe <ragiop07@gmail.com>
2020-04-06 11:23:28 +0200

    added docs

---
+++ docs/build/precache-manifest.604ce86110353be04b5a4b4751b60260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfaba1b655aba26549838fd32cc2bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08772f930fabf5b2cbd2",
+    "url": "/static/css/main.24a946cf.chunk.css"
+  },
+  {
+    "revision": "a138ac684c6a25e59dc0",
+    "url": "/static/js/2.d909a870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d909a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08772f930fabf5b2cbd2",
+    "url": "/static/js/main.e2f390ec.chunk.js"
+  },
+  {
+    "revision": "e9b05a0c4ecc4d34f1f8",
+    "url": "/static/js/runtime-main.adf00006.js"
+  }
+]);
