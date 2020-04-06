58966a3ad27791d1bf25a8d18b236589b4b0fa4f
orYoffe <ragiop07@gmail.com>
2020-04-06 11:36:30 +0200

    added homepage as base url

---
+++ docs/precache-manifest.caa4d4ccb623db57bde1388628bcd577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f032d16f1825db30f20c965211c9a0",
+    "url": "/brainjs-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a76f27772a5e7ee43069",
+    "url": "/brainjs-tic-tac-toe/static/css/main.24a946cf.chunk.css"
+  },
+  {
+    "revision": "a138ac684c6a25e59dc0",
+    "url": "/brainjs-tic-tac-toe/static/js/2.d909a870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brainjs-tic-tac-toe/static/js/2.d909a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76f27772a5e7ee43069",
+    "url": "/brainjs-tic-tac-toe/static/js/main.f4ed646c.chunk.js"
+  },
+  {
+    "revision": "96a5138a56ce7d263525",
+    "url": "/brainjs-tic-tac-toe/static/js/runtime-main.4a0275a8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.604ce86110353be04b5a4b4751b60260.js"
+  "/brainjs-tic-tac-toe/precache-manifest.caa4d4ccb623db57bde1388628bcd577.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brainjs-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
